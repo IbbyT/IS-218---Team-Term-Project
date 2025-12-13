66af64c30d2ea05fb1cba111618fd80efad7b835
IbbyT <iibraheemtahir@gmail.com>
2025-12-13 12:06:30 -0500

    Add starter views and URL routing

---
+++ products/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = "products"
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("products/", views.product_list, name="list"),
+    path("products/<int:pk>/", views.product_detail, name="detail"),
+]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Bakery Review Site Home")
+
+def product_list(request):
+    return HttpResponse("Product list page (pies, cakes, sweets...)")
+
+def product_detail(request, pk):
+    return HttpResponse(f"Product detail page for product #{pk}")
