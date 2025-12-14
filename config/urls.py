1676ed53fee62c479fba0ee69c05dd9a2630d697
Bonyoze <59924045+Bonyoze@users.noreply.github.com>
2025-12-14 00:31:44 -0500

    add media url and root

--- config/urls.py
+++ config/urls.py
@@ -18,13 +18,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("products.urls")),
     path("", include("blog.urls")),
-    path("", include("reviews.urls")),      
+    path("", include("reviews.urls")),
     # homepage + product routes
 ]
+
 urlpatterns += staticfiles_urlpatterns()
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
