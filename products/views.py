775f25b430f6691465c9ebade842153ab98b80ce
Bonyoze <59924045+Bonyoze@users.noreply.github.com>
2025-12-14 16:39:55 -0500

    make products page use products from backend

--- products/views.py
+++ products/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Product
 
 # HOME PAGE
 def home(request):
-    return render(request, "products/home.html")
+	return render(request, "products/home.html")
 
 
 # PRODUCTS LIST PAGE
 def product_list(request):
-    return render(request, "products/product_list.html")
+	products = Product.objects.all()
+	return render(request, "products/product_list.html", {
+		"products": products
+	})
 
 
 # PRODUCT DETAIL PAGE (placeholder for now)
 def product_detail(request, pk):
-    return HttpResponse(f"Product detail page for product #{pk}")
+	return HttpResponse(f"Product detail page for product #{pk}")
