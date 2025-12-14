6184abdf10d6a689c0784877a066767192d2574e
IbbyT <iibraheemtahir@gmail.com>
2025-12-14 12:25:01 -0500

    Add products page with bakery items and update navbar

--- products/views.py
+++ products/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 
+
+# HOME PAGE
 def home(request):
-    return render(request, "products/home.html")    
+    return render(request, "products/home.html")
 
+
+# PRODUCTS LIST PAGE
 def product_list(request):
-    return HttpResponse("Product list page (pies, cakes, sweets...)")
+    return render(request, "products/product_list.html")
 
+
+# PRODUCT DETAIL PAGE (placeholder for now)
 def product_detail(request, pk):
     return HttpResponse(f"Product detail page for product #{pk}")
