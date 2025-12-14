406882629f27a5cc301a9d5891eebbad2ba88115
IbbyT <iibraheemtahir@gmail.com>
2025-12-14 12:54:50 -0500

    Reviews page made

--- reviews/views.py
+++ reviews/views.py
@@ -1,17 +1,16 @@
-from django.shortcuts import render
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect
 from reviews.forms import ReviewsForm
-
-# Create your views here.
-def contact(request):
-    return render(request, "reviews/contact.html" ) 
 
 def review(request):
     form = ReviewsForm(request.POST or None)
-        
+
     if request.method == "POST":
         if form.is_valid():
-            form.save
-            return redirect("home")
-    else:
-        return render(request, "reviews/review.html", {"form": form})
+            form.save()
+            return redirect("home")  # or redirect("reviews:review")
+
+    return render(request, "reviews/review.html", {"form": form})
+
+
+def contact(request):
+    return render(request, "reviews/contact.html")
